e42b854479b7b15b6b09db7117ca5d2bcc491e54
Joao Victor Otto <ottojvn@gmail.com>
2021-05-19 19:22:21 -0300

    JavaScript exercises

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,47 +1,56 @@
 import React from 'react'
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
 
   return (
     <div>
-      <Header course={course} />
-      <Content name1={part1} exercises1={exercises1}
-        name2={part2} exercises2={exercises2}
-        name3={part3} exercises3={exercises3} />
-      <Total part1={exercises1} part2={exercises2} part3={exercises3} />
-    </div>
+      <Header name={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
+    </div >
   )
 }
 
 const Header = (course) => {
-  return (<h1>{course.course}</h1>);
+  return (<h1>{course.name}</h1>);
 }
 
-const Content = (section) => {
+const Content = (parts) => {
   return (
     <div>
-      <Part name={section.name1} exercises={section.exercises1} />
-      <Part name={section.name2} exercises={section.exercises2} />
-      <Part name={section.name3} exercises={section.exercises3} />
+      <Part part={parts.parts[0]} />
+      <Part part={parts.parts[1]} />
+      <Part part={parts.parts[2]} />
     </div>);
 
 }
 
 const Part = (part) => {
-  return (<p>{part.name} {part.exercises}</p>);
+  return (<p>{part.part.name} {part.part.exercises}</p>);
 }
 
-const Total = (exercises) => {
+const Total = (parts) => {
+  const total = parts.parts[0].exercises + parts.parts[1].exercises + parts.parts[2].exercises
   return (
     <p>
-      Number of exercises {exercises.part1 + exercises.part2 + exercises.part3}
+      Number of exercises {total}
     </p>
   )
 }
