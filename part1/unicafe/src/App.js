44235fa32b5536fc6d7a0aad8aeaa56e58b37189
Joao Victor Otto <ottojvn@gmail.com>
2021-05-21 17:36:13 -0300

    Refactor statistics into it's own components

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -7,6 +7,35 @@ const Button = (props) => (
 
 const Display = props => <div>{props.value}</div>
 
+const Statistic = props => <p>{props.text} {props.value}</p>
+
+const Statistics = props => {
+  let total = props.good + props.bad + props.neutral;
+
+
+  if (total > 0) {
+    return (
+      <div>
+        <h1>statistics</h1>
+        <Statistic text="good" value={props.good} />
+        <Statistic text="neutral" value={props.neutral} />
+        <Statistic text="bad" value={props.bad} />
+        <Statistic text="all" value={total} />
+        <Statistic text="average" value={(props.good - props.bad) / total} />
+        <Statistic text="positive" value={`${props.good / total * 100} %`} />
+      </div>
+    )
+  } else {
+    return (
+      <div>
+        <h1>statistics</h1>
+        <p>No feedback given</p>
+      </div>
+    )
+  }
+}
+
+
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
@@ -19,13 +48,7 @@ const App = () => {
       <Button handleClick={() => setGood(good + 1)} text="good" />
       <Button handleClick={() => setNeutral(neutral + 1)} text="neutral" />
       <Button handleClick={() => setBad(bad + 1)} text="bad" />
-      <h1>statistics</h1>
-      <p>good {good}</p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
-      <p>all {good + neutral + bad}</p>
-      <p>average {(good - bad) / (good + neutral + bad)}</p>
-      <p>positive {good / (good + neutral + bad) * 100} %</p>
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
   )
 }
