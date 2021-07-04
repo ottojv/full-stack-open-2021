2d8432f3f41954bdeeb5c2a9ecc36b1f2f9e65cc
Joao Victor Otto <ottojvn@gmail.com>
2021-07-04 10:49:05 -0300

    Use axios to fetch initial app state from server

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,13 +1,19 @@
-import React, { useState } from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+
 import Filter from "./components/Filter";
 import PersonForm from "./components/PersonForm";
 import Persons from "./components/Persons";
 
 const App = () => {
-  const [persons, setPersons] = useState([{ name: "Arto Hellas", number: "012-3456789" }]);
+  const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
   const [keyword, setKeyword] = useState("");
+
+  useEffect(() => {
+    axios.get("http://localhost:3001/persons").then((response) => setPersons(response.data));
+  }, []);
 
   const addPerson = (event) => {
     event.preventDefault();
